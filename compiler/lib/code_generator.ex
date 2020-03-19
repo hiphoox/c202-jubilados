eac2cffa78316a7cbdecfea6205ec65f8864e16e
Oswaldo_Hernández_Escobar <oswaldo.metallica97@gmail.com>
2020-03-19 13:53:55 -0600

    Modularización Code Generator

--- compiler/lib/code_generator.ex
+++ compiler/lib/code_generator.ex
@@ -1,19 +1,24 @@
 defmodule CodeGenerator do
-  def generate_code(ast) do
-    code = post_order(ast)
-    IO.puts("\nCode Generator output:")
-    IO.puts(code)
-    code
+  def generate_code(astTree, flag, path) do
+    if is_map(astTree) do
+      code = post_order(astTree)
+      parsing_flag(code, flag)
+  else 
+      IO.puts("Syntax Error")
+      {_,_,error_line,atom} = astTree
+      error_line = to_string(error_line+1)
+      error_reason = to_string(atom)
+      error_message = "Error:  " <> error_line <> " : "<> error_reason
+      {:error, error_message}
+    end
   end
 
   def post_order(node) do
     case node do
       nil ->
         nil
-
       ast_node ->
         code_snippet = post_order(ast_node.left_node)
-        # TODO: Falta terminar de implementar cuando el arbol tiene mas ramas
         post_order(ast_node.right_node)
         emit_code(ast_node.node_name, code_snippet, ast_node.value)
     end
@@ -45,4 +50,17 @@ defmodule CodeGenerator do
   def emit_code(:constant, _code_snippet, value) do
     "$#{value}"
   end
+
+  def parsing_flag(asm, flag) do
+    if flag == :show_asm do
+      IO.puts("\nCode Generator output:")
+      IO.puts(asm)
+      {:asm, asm}
+    else
+      IO.puts("\nCode  output:")
+      IO.puts(asm)
+      {:ok, asm}
+    end
+  end
+
 end
