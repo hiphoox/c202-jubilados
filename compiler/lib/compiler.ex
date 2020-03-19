1de3d543e84c90e324c08abba36d31e2b72dadff
Coral Rodriguez <coralrodriguez@rappi.com>
2020-03-19 13:22:11 -0600

    .

--- compiler/lib/compiler.ex
+++ compiler/lib/compiler.ex
@@ -2,68 +2,31 @@ defmodule Compiler do
   @moduledoc """
   Documentation for `Compiler`.
   """
-@commands %{
-    "h" => "Prints this help",
-  }
 
   def main(args) do
-    args
-    |> parse_args
-    |> process_args
-  end
-
-  def parse_args(args) do
-    OptionParser.parse(args, switches: [h: :boolean])
-  end
-
-  defp process_args({[h: true], _, _}) do
-    init_help()
-  end
-
-  defp process_args({_, [file_name], _}) do
-    compile_file(file_name)
-  end
-
-  def compile_file(file_path) do
-    IO.puts("Compiling file: " <> file_path)
-    assembly_path = String.replace_trailing(file_path, ".c", ".s")
-
-    tokens_list = File.read!(file_path)
-    |> clean_code()
-    |> Scanner.scan_words()
-
-    result = evaluator_lexer(tokens_list)
-    if result == [] do
-      astTree = tokens_list
-      |> Parser.parse_program()
-
-      if is_map(astTree) do
-        astTree
-        |> CodeGenerator.generate_code()
-        |> Linker.generate_binary(assembly_path)
-        |> IO.inspect()
-      end
-
-      if is_tuple(astTree) do
-        IO.puts("Syntax Error")
-        {_,_,error_line,atom} = astTree
-        error_line = to_string(error_line+1)
-        error_reason = to_string(atom)
-        error_message = "Error:  " <> error_line <> " : "<> error_reason
-        IO.inspect(error_message)
-      end
-
-    else
-      IO.puts("Lexical Error:")
-      [_,word_error,line] = result
-      error_line = to_string(line+1)
-      error_message = "Error: " <> error_line <> ":is not expected: " <> word_error
-      IO.inspect(error_message)
+    case args do
+      ["-h"] -> init_help() |> IO.puts();
+      [path] -> if path =~ ".c", do: compile_file(path, :no_output), else: show_error(1) |> IO.puts;
+      ["-t", path] -> compile_file(path, :show_token); 
+      ["-s", path] -> compile_file(path, :show_asm); 
+      ["-a", path] -> compile_file(path, :show_ast);
+      ["-o", path, new_name] -> compile_file(path, new_name);
+      _ -> show_error(1) |> IO.puts;
+           show_error(1)
     end
+  end
+
+  def compile_file(file_path, flag) do
+      if file_path =~ ".c" and File.exists?(file_path) do
+       Orchestrator.manager(clean_code(File.read!(file_path)), file_path, flag);
+     else
+       show_error(3) |> IO.puts;
+       show_error(3);
+     end
 end  
 
   def clean_code(file_content) do
-    divide_code = String.trim(file_content)
+    divide_code = String.trim(file_content) 
     clean_words = Regex.split(~r/\n/,divide_code)
     counter = 0
     get_tokens(clean_words,counter)
@@ -83,27 +46,19 @@ end
     []
   end
 
-  def evaluator_lexer(tokens_list) when tokens_list != [] do
-      head= hd tokens_list
-      tail= tl tokens_list
-      if is_list(head) do
-        head
-      else
-        evaluator_lexer(tail)
-      end
-  end
-
-  def evaluator_lexer(tokens_list)  do
-    []
-  end
-
   defp init_help do
     IO.puts("\nnqcc --h file_name \n")
 
     IO.puts("\nThe compiler supports following options:\n")
 
-    @commands
-    |> Enum.map(fn {command, description} -> IO.puts("  #{command} - #{description}") end)
   end
+
+    def show_error(num) do
+     case num do
+       1 -> "Escriba -h para la ayuda." 
+       2 -> "Comando(s) no válido. Escriba -h para la ayuda."
+       3 -> "Archivo inválido o no existe en el directorio." 
+     end
+   end
 
 end
