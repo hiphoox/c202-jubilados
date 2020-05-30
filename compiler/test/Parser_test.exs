a4619600f280d04e31a142e450a1c1e6d784fd93
Oswaldo_Hernández_Escobar <oswaldo.metallica97@gmail.com>
2020-05-29 22:46:14 -0500

    Test scanner and parser

--- compiler/test/Parser_test.exs
+++ compiler/test/Parser_test.exs
@@ -170,6 +170,244 @@ defmodule ParserTest do
       {{:constant, 5}, 1},
       {:semicolon, 1},
       {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 1}, 1},
+      {:add_operator, 1},
+      {{:constant, 2}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+
+       ast_binary_op1: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 1}, 1},
+      {:neg_operator, 1},
+      {{:constant, 2}, 1},
+      {:neg_operator, 1},
+      {{:constant, 3}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op2: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 6}, 1},
+      {:div_operator, 1},
+      {{:constant, 3}, 1},
+      {:div_operator, 1},
+      {{:constant, 2}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op3: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 4}, 1},
+      {:div_operator, 1},
+      {{:constant, 2}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op4: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {:neg_operator, 1},
+      {{:constant, 12}, 1},
+      {:div_operator, 1},
+      {{:constant, 5}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op5: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:mult_operator, 1},
+      {{:constant, 3}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op6: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:mult_operator, 1},
+      {{:constant, 3}, 1},
+      {:add_operator, 1},
+      {{:constant, 4}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op7: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 4}, 1},
+      {:mult_operator, 1},
+      {{:constant, 5}, 1},
+      {:add_operator, 1},
+      {{:constant, 6}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op8: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 1}, 1},
+      {:neg_operator, 1},
+      {{:constant, 2}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+      ast_binary_op9: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:neg_operator, 1},
+      {:neg_operator, 1},
+      {{:constant, 1}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+      ast_binary_op10: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {:bitwise_operator, 1},
+      {{:constant, 2}, 1},
+      {:add_operator, 1},
+      {{:constant, 3}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+      ast_binary_op11: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {:bitwise_operator, 1},
+      {{:constant, 1}, 1},
+      {:add_operator, 1},
+      {{:constant, 1}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op12: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:open_paren, 1},
+      {:neg_operator, 1},
+      {{:constant, 1}, 1},
+      {:close_paren, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+       ast_binary_op13: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {:div_operator, 1},
+      {{:constant, 3}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+
+       ast_binary_op14: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 1}, 1},
+      {:add_operator, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output),
+
+
+       ast_binary_op15: Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:mult_operator, 1},
+      {{:constant, 2}, 1},
+      {:close_brace, 2}
       ], :no_output)}
   end
   
@@ -337,8 +575,213 @@ defmodule ParserTest do
       ], :no_output) == state[:ast_unary_op7]
   end  
 
+  test "Test 11. Suma", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 1}, 1},
+      {:add_operator, 1},
+      {{:constant, 2}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op]
+  end  
+
+  test "Test 12. Resta", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 1}, 1},
+      {:neg_operator, 1},
+      {{:constant, 2}, 1},
+      {:neg_operator, 1},
+      {{:constant, 3}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op1]
+  end  
+
+   test "Test 13. Div_1", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 6}, 1},
+      {:div_operator, 1},
+      {{:constant, 3}, 1},
+      {:div_operator, 1},
+      {{:constant, 2}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op2]
+  end  
+
+   test "Test 14. Div_2", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 4}, 1},
+      {:div_operator, 1},
+      {{:constant, 2}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op3]
+  end  
+
+   test "Test 15. Div_Neg", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {:neg_operator, 1},
+      {{:constant, 12}, 1},
+      {:div_operator, 1},
+      {{:constant, 5}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op4]
+  end  
+
+  test "Test 16. Multi", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:mult_operator, 1},
+      {{:constant, 3}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op5]
+  end  
+
+  test "Test 17. Varias_Op", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:mult_operator, 1},
+      {{:constant, 3}, 1},
+      {:add_operator, 1},
+      {{:constant, 4}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op6]
+  end  
+
+   test "Test 18. Precedence", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 4}, 1},
+      {:mult_operator, 1},
+      {{:constant, 5}, 1},
+      {:add_operator, 1},
+      {{:constant, 6}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op7]
+  end  
+
+  test "Test 19. Sub", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 1}, 1},
+      {:neg_operator, 1},
+      {{:constant, 2}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op8]
+  end  
+
+  test "Test 20. Sub_neg", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:neg_operator, 1},
+      {:neg_operator, 1},
+      {{:constant, 1}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op9]
+  end  
+
+  test "Test 21. Bit_add", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {:bitwise_operator, 1},
+      {{:constant, 2}, 1},
+      {:add_operator, 1},
+      {{:constant, 3}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op10]
+  end  
+
+  test "Test 22. Paren_Bit_add", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {:bitwise_operator, 1},
+      {{:constant, 1}, 1},
+      {:add_operator, 1},
+      {{:constant, 1}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op11]
+  end  
+
   #Test_to_Failed
-  test "Test 11. Negation wrong order", state do
+
+  test "Test 23. Negation wrong order", state do
     assert Parser.init_parse([
       {:int_keyword, 0},
       {:main_keyword, 0},
@@ -353,7 +796,7 @@ defmodule ParserTest do
       ], :no_output) == state[:ast_unary_op8]
   end  
 
-  test "Test 12. Mising ret_val", state do
+  test "Test 24. Mising ret_val", state do
     assert Parser.init_parse([
       {:int_keyword, 0},
       {:main_keyword, 0},
@@ -367,5 +810,68 @@ defmodule ParserTest do
       {:semicolon, 1},
       {:close_brace, 2}
       ], :no_output) == state[:ast_unary_op10]
+  end  
+
+    test "Test 25. Mal_paren", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:open_paren, 1},
+      {:neg_operator, 1},
+      {{:constant, 3}, 1},
+      {:close_paren, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op12]
+  end  
+
+   test "Test 26. Missing_first_op", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {:div_operator, 1},
+      {{:constant, 3}, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op13]
+  end  
+
+    test "Test 27. Missing_second_op", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 1}, 1},
+      {:add_operator, 1},
+      {:semicolon, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op14]
+  end  
+
+   test "Test 28. No_semicolon", state do
+    assert Parser.init_parse([
+      {:int_keyword, 0},
+      {:main_keyword, 0},
+      {:open_paren, 0},
+      {:close_paren, 0},
+      {:open_brace, 0},
+      {:return_keyword, 1},
+      {{:constant, 2}, 1},
+      {:mult_operator, 1},
+      {{:constant, 2}, 1},
+      {:close_brace, 2}
+      ], :no_output) == state[:ast_binary_op15]
   end  
 end
