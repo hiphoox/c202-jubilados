c616e8d8c60fb2f723f0a982bfb1d26d6cb5a71c
Coral Rodriguez <coralrodriguez@rappi.com>
2020-06-18 00:00:20 -0500

    create module Utils

--- compiler/lib/compiler.ex
+++ compiler/lib/compiler.ex
@@ -18,33 +18,12 @@ defmodule Compiler do
 
   def compile_file(file_path, flag) do
       if file_path =~ ".c" and File.exists?(file_path) do
-       Orchestrator.manager(clean_code(File.read!(file_path)), file_path, flag);
+       Orchestrator.manager(Utils.clean_code(File.read!(file_path)), file_path, flag);
      else
        show_error(3) |> IO.puts;
        show_error(3);
      end
 end  
-
-  def clean_code(file_content) do
-    divide_code = String.trim(file_content) 
-    clean_words = Regex.split(~r/\n/,divide_code)
-    counter = 0
-    get_tokens(clean_words,counter)
- end
-
-  def get_tokens(program,line) when program != []  do
-    lines = hd program
-    extra_lines = tl program
-    tokens = Regex.split(~r/\s+/, lines, trim: true)
-    tokens_in_line = Enum.flat_map(tokens, fn token -> [{token,line}] end)
-    line = line + 1
-    others = get_tokens(extra_lines,line)
-    tokens_in_line ++ others
-  end
-
-  def get_tokens(_program,_linea)  do
-    []
-  end
 
   defp init_help do
     IO.puts("\nnqcc --h file_name \n")
